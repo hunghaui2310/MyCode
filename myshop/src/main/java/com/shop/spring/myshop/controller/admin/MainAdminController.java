f3d6fd6d0fa0e30076ab99cd3107536e0ecad359
Hung <hunghaui2310@gmail.com>
2019-10-04 16:42:47 +0700

    commit

--- myshop/src/main/java/com/shop/spring/myshop/controller/admin/MainAdminController.java
+++ myshop/src/main/java/com/shop/spring/myshop/controller/admin/MainAdminController.java
@@ -1,5 +1,7 @@
 package com.shop.spring.myshop.controller.admin;
 
+import com.shop.spring.myshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainAdminController {
 
+    @Autowired
+    ProductService productService;
+
     @RequestMapping("/admin/product")
     public String getProduct(Model model){
+        model.addAttribute("listProduct", productService.getAll());
         return "admin/product_list";
     }
 }
